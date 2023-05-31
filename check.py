806fb3be0bbde42e9c03e95a2be1c66e05c53d05
madgIitch <peorrr9@gmail.com>
2023-05-31 22:50:54 +0200

    echale un vistazo

--- check.py
+++ check.py
@@ -24,8 +24,8 @@ def check():
 #Works!!!!!!
 
 def check2():
-    keys = "That chocolate lava cake was divine! Already craving it again. We should definitely explore more dessert places soon."
-    secret = "6550524d45050d5f060954501507165e5414571900505f501047554418555b46580f501016750a4b0004074a43521605445d0c06455f1611035f575d0b18446e5019470e5f105e064150575208085840065b4c455d4044090e435c180e5e4353415c00154303431241115d025b061017405f595e48"
+    keys = "Perfect! See you soon, MJ. Have an amazing day! Can't wait for our picnic. It's going to be wonderful. See you soon!"
+    secret = "695c475f545a4419416404551048571011170d580f1414792f1d167100120645075d125854574d0a0c5e18505949154527075717411941020a4d445e5e4a1357444419465b050c51064c4271151643455e5c0b575619405c12035644150e5707531407445f4c12615d04424a094212460a5a5f40"
 
     def encrypt(key, secret):
         crypted = ''.join([chr(ord(c) ^ ord(secret[i])) for i, c in enumerate(key[:len(secret)])])
@@ -56,5 +56,22 @@ def check3():
 
 
 def check4():
-    encriptado = 
-    plano = ""
+    descifrado = []
+    with open('cifradoRestante.txt', 'r') as file:
+        cifrado = file.readlines()
+    cifrado = [entry.strip() for entry in cifrado]
+
+
+    def decrypt(key, secret):
+        crypted = ''.join(
+            [chr(ord(c) ^ ord(secret[i])) for i, c in enumerate(key[:len(secret)])])
+        return crypted
+
+    for cif in cifrado:
+        descifrado.append(decrypt(cif, check2()))
+
+    print(descifrado)
+    return descifrado
+
+check4()
+
