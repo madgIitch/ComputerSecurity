9396c1036036442aff4890881fdba17fcc932e43
Pepe Ortiz Roldán <99665744+madgIitch@users.noreply.github.com>
2023-06-19 19:29:59 -0400

    prog2 funcionando

--- proyect2/prog2.pl
+++ proyect2/prog2.pl
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 
-my $program = './program';  # Path to the compiled program
+my $program = './prog2';  # Path to the compiled program
 my $payload = 'A' x 10;     # String that exceeds the buffer size (10 characters)
 
 system($program, $payload);
