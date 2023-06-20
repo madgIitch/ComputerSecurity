12f1e6faee3549a3b60261431574e5ff90f19db6
madgIitch <peorrr9@gmail.com>
2023-06-21 01:39:23 +0200

    más ralldo que un adolescente con twitter

--- proyect2/hello.asm
+++ proyect2/hello.asm
@@ -1,18 +1,44 @@
-section .data
-    message db 'hola chavales',0
-
 section .text
     global _start
 
 _start:
-    ; Escribir la cadena en la salida estándar
+    ; Imprimir el mensaje "Ingrese su entrada: "
     mov eax, 4
     mov ebx, 1
-    mov ecx, message
-    mov edx, 11
+    mov ecx, mensaje1
+    mov edx, mensaje1len
     int 0x80
 
-    ; Salir del programa
+    ; Leer la entrada del usuario
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, buffer
+    mov edx, 32
+    int 0x80
+
+    ; Realizar el buffer overflow y sobrescribir el valor de retorno
+    lea edi, [esp+0x20]
+    add edi, 12
+    mov dword [edi], 0x6c6c6568
+
+    ; Imprimir el mensaje "hello world"
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, mensaje2
+    mov edx, mensaje2len
+    int 0x80
+
+    ; Finalizar el programa
     mov eax, 1
     xor ebx, ebx
     int 0x80
+
+section .data
+    mensaje1 db "Ingrese su entrada: ", 0
+    mensaje1len equ $ - mensaje1
+
+    mensaje2 db "hello world", 0
+    mensaje2len equ $ - mensaje2
+
+section .bss
+    buffer resb 32
